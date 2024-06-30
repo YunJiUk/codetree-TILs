20e4894415b31b148bd2e439121721a1d0c41029
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-01 01:28:16 +0900

    Title: 배열 회전, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240701/배열 회전/array-rotation.cpp
+++ 240701/배열 회전/array-rotation.cpp
@@ -5,57 +5,56 @@
 using namespace std;
 
 int num1, num2, num3;
-
 int Map[330][330];
 int ansMap[330][330];
 
-struct Node{
+struct Node {
     int y;
     int x;
 };
 
-
-void init(){
-
+void init() {
+    // 초기화 로직이 필요하면 여기에 작성
 }
 
-void input(){
+void input() {
     cin >> num1 >> num2 >> num3;
 
-    for(int n1 = 0; n1 < num1; n1++){
-        for(int n2 = 0; n2 < num2; n2++){
+    for (int n1 = 0; n1 < num1; n1++) {
+        for (int n2 = 0; n2 < num2; n2++) {
             cin >> Map[n1][n2];
         }
     }
-
 }
 
-void solve(){
+void solve() {
     int cnt = 0;
     int checky = num1;
     int checkx = num2;
-    while(1){
 
+    while (true) {
         deque<int> q1;
         deque<Node> q2;
-        for(int n1 = cnt; n1 < (num1 - cnt); n1++){
-            q1.push_back(Map[n1][cnt]);
-            q2.push_back({n1, cnt});
-        }
-        
-        for(int n1 = cnt + 1; n1 < (num2 - cnt - 1); n1++){
-            q1.push_back(Map[num1 - 1 - cnt][n1]);
-            q2.push_back({num1 - 1 - cnt, n1});
+
+        // 각 layer의 테두리 요소들을 q1, q2에 삽입
+        for (int n1 = cnt; n1 < num2 - cnt; n1++) {
+            q1.push_back(Map[cnt][n1]);
+            q2.push_back({cnt, n1});
         }
 
-        for(int n1 = num1 - 1 - cnt; n1 >= cnt; n1--){
+        for (int n1 = cnt + 1; n1 < num1 - cnt; n1++) {
             q1.push_back(Map[n1][num2 - cnt - 1]);
             q2.push_back({n1, num2 - cnt - 1});
         }
 
-        for(int n1 = num2 - 2 - cnt; n1 >= (1 + cnt); n1--){
-            q1.push_back(Map[cnt][n1]);
-            q2.push_back({cnt, n1});
+        for (int n1 = num2 - cnt - 2; n1 >= cnt; n1--) {
+            q1.push_back(Map[num1 - cnt - 1][n1]);
+            q2.push_back({num1 - cnt - 1, n1});
+        }
+
+        for (int n1 = num1 - cnt - 2; n1 > cnt; n1--) {
+            q1.push_back(Map[n1][cnt]);
+            q2.push_back({n1, cnt});
         }
 
         cnt++;
@@ -63,26 +62,20 @@ void solve(){
         int len = q1.size();
         int mv = num3 % len;
 
-        for(int n1 = mv; n1 < q2.size(); n1++){
-            ansMap[q2[n1].y][q2[n1].x] = q1.front();
-            q1.pop_front();
+        // 회전한 값을 ansMap에 삽입
+        for (int n1 = 0; n1 < len; n1++) {
+            int idx = (n1 + mv) % len;
+            ansMap[q2[n1].y][q2[n1].x] = q1[idx];
         }
 
-        for(int n1 = 0; n1 < mv; n1++){
-            ansMap[q2[n1].y][q2[n1].x] = q1.front();
-            q1.pop_front();
-        }
+        checky -= 2;
+        checkx -= 2;
 
-        checky = checky - 2;
-        checkx = checkx - 2;
-
-
-        if(checky <= 0 || checkx <= 0) break;
+        if (checky <= 0 || checkx <= 0) break;
     }
 
-
-    for(int n1 = 0; n1 < num1; n1++){
-        for(int n2 = 0; n2 < num2; n2++){
+    for (int n1 = 0; n1 < num1; n1++) {
+        for (int n2 = 0; n2 < num2; n2++) {
             cout << ansMap[n1][n2] << " ";
         }
         cout << endl;
@@ -90,12 +83,8 @@ void solve(){
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     init();
-
     input();
-
     solve();
-
     return 0;
 }
