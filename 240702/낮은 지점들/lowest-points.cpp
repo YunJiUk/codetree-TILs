a8a1bf8f887819a90f965c7f16ba6c30ab10922c
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-02 21:14:05 +0900

    Title: 낮은 지점들, Time: 55ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240702/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int num1, num2, num3;
+
+unordered_map<int, int> um1;
+
+void init(){
+
+}
+
+void input(){
+    cin >> num1;
+
+    for(int n1 = 0; n1 < num1; n1++){
+        cin >> num2 >> num3;
+        if(um1[num2] == 0){
+            um1[num2] = num3;
+        }
+        else if(um1[num2] > num3){
+            um1[num2] = num3;
+        }
+    }
+}
+
+void solve(){
+    int ans = 0;
+    for(auto item : um1){
+        ans += item.second;
+    }
+
+    cout << ans;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    init();
+
+    input();
+
+    solve();
+    
+    return 0;
+}
