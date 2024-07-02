86801fc327cbac9b9d9eeaa00c7d7ea4968a4b6c
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-02 20:37:30 +0900

    Title: 자주 등장한 top k 숫자, Time: 13ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240702/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int num1, num2, num3;
+
+unordered_map<int, int> um1;
+
+struct Node{
+    int num;
+    int cnt;
+};
+
+vector<Node> v1;
+
+bool sortFunc(Node left, Node right){
+    if(left.cnt > right.cnt) return true;
+    else if(left.cnt == right.cnt){
+        if(left.num > right.num) return true;
+    }
+    return false;
+}
+
+void init(){
+
+}
+
+void input(){
+    cin >> num1 >> num2;
+    v1.resize(num1);
+    for(int n1 = 0; n1 < num1; n1++){
+        cin >> num3;
+        um1[num3]++;
+        v1[n1] = {num3, um1[num3]};
+    }
+}
+
+void solve(){
+    sort(v1.begin(), v1.end(), sortFunc);
+
+    for(int n1 = 0; n1 < num2; n1++){
+        cout << v1[n1].num << " ";
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    
+    init();
+
+    input();
+
+    solve();
+
+
+    return 0;
+}
