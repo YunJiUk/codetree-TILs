aa75be2e42a4e68b74489fe48ad21ff8eb43a73e
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-03 10:17:47 +0900

    Title: 데이터 비교, Time: 226ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240703/데이터 비교/data-comparison.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+unordered_set<int> us1;
+
+int num1;
+int num2;
+vector<int> v1;
+vector<int> v2;
+
+void init(){
+
+}
+
+void input(){
+    cin >> num1;
+    for(int n1 = 0; n1 < num1; n1++){
+        cin >> num2;
+        us1.insert(num2);
+    }
+
+    cin >> num1;
+    for(int n1 = 0; n1 < num1; n1++){
+        cin >> num2;
+        if(us1.find(num2) == us1.end()){
+            cout << 0 << " ";
+        }
+        else{
+            cout << 1 << " ";
+        }
+    }
+}
+
+void solve(){
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    init();
+
+    input();
+
+    solve();
+
+    return 0;
+}
