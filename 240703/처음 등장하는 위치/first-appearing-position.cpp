31e97a79930c800a3f5d25901226d2dadf4baea7
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-03 09:26:21 +0900

    Title: 처음 등장하는 위치, Time: 184ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240703/처음 등장하는 위치/first-appearing-position.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+map<int, int> m1;
+
+int num1, num2;
+
+void init(){
+
+}
+
+void input(){
+    cin >> num1;
+    for(int n1 = 1; n1 <= num1; n1++){
+        cin >> num2;
+        if(m1[num2] == 0){
+            m1[num2] = n1;
+        }
+    }
+}
+
+void solve(){
+    for(auto item : m1){
+        cout << item.first << " " << item.second << endl;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    init();
+
+    input();
+
+    solve();
+
+    return 0;
+}
