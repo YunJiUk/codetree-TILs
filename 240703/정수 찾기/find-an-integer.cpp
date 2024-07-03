8760eaa22065656c95711375b4f44eed831c0a27
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-03 10:27:23 +0900

    Title: 정수 찾기, Time: 223ms, Memory: 3MB, Status: Passed - CodeTree

--- 240703/정수 찾기/find-an-integer.cpp
+++ 240703/정수 찾기/find-an-integer.cpp
@@ -3,47 +3,30 @@
 
 using namespace std;
 
-int num1, num2;
-unordered_set<int> us1;
-
-void init(){
-
-}
-
-void input(){
-    cin >> num1;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin >> num2;
-        us1.insert(num2);
-    }
-
-    cin >> num1;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin>> num2;
-        if(us1.find(num2) == us1.end()){
-            cout << 0 << endl;
-        }
-        else{
-            cout << 1 << endl;
-        }
-    }
-}
-
-void solve(){
-
-}
+// 변수 선언
+int n, m;
+unordered_set<int> s;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    // 입력:
+    cin >> n;
 
-    init();
+    // 수열 a의 원소를 전부 HashSet에 넣어줍니다.
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
 
-    input();
+    cin >> m;
 
-    solve();
+    // 수열 b의 각 원소가 수열 a에 있는지 확인합니다.
+    for(int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
 
+        // 수열 a 안에 있다면 1을, 없다면 0을 출력합니다.
+        cout << (int)(s.find(x) != s.end()) << endl;
+    }
     return 0;
 }
