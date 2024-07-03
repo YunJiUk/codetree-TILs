958f4933f14d15e25b16b3f48fb6b38972cda8d8
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-03 10:20:01 +0900

    Title: 데이터 비교, Time: 197ms, Memory: 8MB, Status: Passed - CodeTree

--- 240703/데이터 비교/data-comparison.cpp
+++ 240703/데이터 비교/data-comparison.cpp
@@ -4,45 +4,30 @@
 
 using namespace std;
 
-unordered_set<int> us1;
-
-int num1;
-int num2;
-
-void init(){
-
-}
-
-void input(){
-    cin >> num1;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin >> num2;
-        us1.insert(num2);
-    }
-
-    cin >> num1;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin >> num2;
-        if(us1.find(num2) == us1.end()){
-            cout << 0 << " ";
-        }
-        else{
-            cout << 1 << " ";
-        }
-    }
-}
-
-void solve(){
-
-}
-
+int n,m;
+unordered_set<int> s1, s2;
+vector<int> v1, v2;
 int main() {
-    // 여기에 코드를 작성해주세요.
-    init();
+    cin >> n;
+    for(int i = 0 ; i<n; i++){
+        int t; cin >> t;
+        v1.push_back(t);
+        s1.insert(t);
+    }
 
-    input();
+    cin >> m;
+    for(int i = 0 ; i<m; i++){
+        int t; cin >> t;
+        v2.push_back(t);
+        s2.insert(t);
+    }
 
-    solve();
+    for(int i = 0; i< v2.size(); i++){
+        if(s1.find(v2[i]) == s1.end()) cout << 0 << " ";
+        else cout << 1 << " ";
+    }
+
+
 
     return 0;
 }
