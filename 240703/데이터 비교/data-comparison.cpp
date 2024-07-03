13f25f9b65797f1c4d41147c5319cefc1571c36a
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-03 10:18:50 +0900

    Title: 데이터 비교, Time: 163ms, Memory: 5MB, Status: Passed - CodeTree

--- 240703/데이터 비교/data-comparison.cpp
+++ 240703/데이터 비교/data-comparison.cpp
@@ -1,50 +1,40 @@
 #include <iostream>
 #include <unordered_set>
-#include <vector>
+
+#define MAX_NUM 100000
 
 using namespace std;
 
-unordered_set<int> us1;
+// 변수 선언
+int n, m;
+int arr1[MAX_NUM];
+int arr2[MAX_NUM];
 
-int num1;
-int num2;
-vector<int> v1;
-vector<int> v2;
-
-void init(){
-
-}
-
-void input(){
-    cin >> num1;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin >> num2;
-        us1.insert(num2);
-    }
-
-    cin >> num1;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin >> num2;
-        if(us1.find(num2) == us1.end()){
-            cout << 0 << " ";
-        }
-        else{
-            cout << 1 << " ";
-        }
-    }
-}
-
-void solve(){
-
-}
+unordered_set<int> set1;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    init();
+    // 입력:
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr1[i];
 
-    input();
-
-    solve();
+    cin >> m;
+    for(int i = 0; i < m; i++)
+        cin >> arr2[i];
+    
+    // 첫 번째 수열의 원소를 전부 HashSet에 넣어줍니다.
+    for(int i = 0; i < n; i++)
+        set1.insert(arr1[i]);
+    
+    // 두 번째 수열의 각 원소가 첫 번째 수열에 들어있는지를 확인합니다.
+    for(int i = 0; i < m; i++) {
+        // 찾지 못했다면 0을 출력합니다.
+        if(set1.find(arr2[i]) == set1.end())
+            cout << 0 << " ";
+        // 찾았다면 1을 출력합니다.
+        else
+            cout << 1 << " ";
+    }
 
     return 0;
 }
