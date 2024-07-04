627d6c83544e0dd3587c862a6a4e9347a2741a05
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-04 17:59:25 +0900

    Title: 대칭 차집합, Time: 256ms, Memory: 10MB, Status: Passed - CodeTree

--- 240704/대칭 차집합/symmetric-difference-set.cpp
+++ 240704/대칭 차집합/symmetric-difference-set.cpp
@@ -1,53 +1,42 @@
 #include <iostream>
 #include <unordered_set>
 
+#define MAX_NUM 200000
+
 using namespace std;
 
-int num1, num2, num3, num4;
+// 변수 선언
+int n, m;
+int A[MAX_NUM];
+int B[MAX_NUM];
+int ans;
 
-unordered_set<int> us1;
-unordered_set<int> us2;
-unordered_set<int> us3;
-
-
-void init(){
-
-}
-
-void input(){
-    cin >> num1 >> num2;
-    for(int n1 = 0; n1 < num1; n1++){
-        cin >> num3;
-        us1.insert(num3);
-    }
-
-    for(int n1 = 0; n1 < num2; n1++){
-        cin >> num4;
-        us2.insert(num4);
-
-        if(us1.find(num4) == us1.end()){
-            us3.insert(num4);
-        }
-    }
-}
-
-void solve(){
-    for(auto item : us1){
-        if(us2.find(item) == us2.end()){
-            us3.insert(item);
-        }
-    }
-
-    cout << us3.size();
-}
+unordered_set<int> s;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    init();
+    // 입력:
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+    for(int i = 0; i < m; i++)
+        cin >> B[i];
+    
+    ans = n + m;
+    
+    // 수열 A의 원소를 전부 HashSet에 넣어줍니다.
+    for(int i = 0; i < n; i++)
+        s.insert(A[i]);
+    
+    // 수열 B의 각 원소가 첫 번째 수열에 들어있는지를 확인합니다.
+    for(int i = 0; i < m; i++) {
+        // 만약 들어있다면 수열 A와 수열 B에 모두 있는 값입니다.
+        // 이는 대칭 차집합의 원소가 아니므로 정답의 개수에서 지워줍니다.
+        if(s.find(B[i]) != s.end())
+            ans -= 2;
+    }
 
-    input();
-
-    solve();
+    // 대칭 차집합의 원소의 개수를 출력합니다.
+    cout << ans;
 
     return 0;
 }
