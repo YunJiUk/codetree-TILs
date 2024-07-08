98818aab674269a328d70c87d48262112ac3e739
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-08 19:03:48 +0900

    Title: treeset 기본, Time: 150ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240708/treeset 기본/treeset-basic.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int num1, num2;
+set<int> s1;
+string str1;
+
+void init(){
+
+}
+
+void input(){
+    cin >> num1;
+
+    for(int n1 = 0; n1 < num1; n1++){
+        cin >> str1;
+
+        if(str1 == "add"){
+            cin >> num2;
+
+            s1.insert(num2);
+        }
+
+        else if(str1 == "remove"){
+            cin >> num2;
+
+            s1.erase(num2);
+        }
+
+        else if(str1 == "find"){
+            cin >> num2;
+
+            if(s1.find(num2) != s1.end()){
+                cout << "true" << endl;
+            }
+            else{
+                cout << "false" << endl;
+            }
+        }
+
+
+        else if(str1 == "lower_bound"){
+            cin >> num2;
+
+            if(s1.lower_bound(num2) != s1.end()){
+                cout << *s1.lower_bound(num2) << endl;
+            }
+            else{
+                cout << "None" << endl;
+            }
+        }
+
+        else if(str1 == "upper_bound"){
+            cin >> num2;
+
+            if(s1.upper_bound(num2) != s1.end()){
+                cout << *s1.upper_bound(num2) << endl;
+            }
+            else{
+                cout << "None" << endl;
+            }
+        }
+
+        else if(str1 == "largest"){
+            if(s1.size() != 0){
+                cout << *s1.rbegin() << endl;
+            }
+
+            else{
+                cout << "None" << endl;
+            }
+        }
+
+        else if(str1 == "smallest"){
+            if(s1.size() != 0){
+                cout << *s1.begin() << endl;
+            }
+
+            else{
+                cout << "None" << endl;
+            }
+        }
+    }
+}
+
+void solve(){
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    init();
+
+    input();
+
+    solve();
+    return 0;
+}
