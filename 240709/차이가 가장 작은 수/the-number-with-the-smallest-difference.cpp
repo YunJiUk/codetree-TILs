0638ea84bc6b084199569066cc1b8543f65b002b
YunJiUk <117324719+YunJiUk@users.noreply.github.com>
2024-07-09 14:49:11 +0900

    Title: 차이가 가장 작은 수, Time: 17ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240709/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
+++ 240709/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -13,34 +13,32 @@ int main() {
 
     for (int n1 = 0; n1 < num1; n1++) {
         cin >> num3;
-        s1.insert(num3);
 
+        // set에 현재 값을 삽입하기 전에 주변 값을 검사합니다.
+        auto item = s1.upper_bound(num3);
 
-        auto item = s1.lower_bound(num3);
-        auto item2 = s1.upper_bound(num3);
+        if (item != s1.end()) {
+            if ((*item - num3) > num2 && (*item - num3) < minans) {
+                minans = *item - num3;
+            }
+        }
 
         if (item != s1.begin()) {
             item--;
-            if ((num3 - *item) >= num2 && (num3 - *item) < minans) {
+            if ((num3 - *item) > num2 && (num3 - *item) < minans) {
                 minans = num3 - *item;
             }
         }
 
-        if (item2 != s1.end()) {
-
-            if ((*item2 - num3) >= num2 && (*item2 - num3) < minans) {
-                minans = *item2 - num3;
-            }
-        }
+        // 새로운 값을 set에 삽입합니다.
+        s1.insert(num3);
     }
 
     if (minans == 2134567890) {
         cout << -1;
-        return 0;
+    } else {
+        cout << minans;
     }
 
-    cout << minans;
-
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
